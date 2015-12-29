d8a597d2cf8a65ee58bdedad84cc006d974ed295
Verbalist <enshteynn@ukr.net>
2015-12-29 17:18:48 +0000

    0.79426

--- main.py
+++ main.py
@@ -7,23 +7,42 @@ from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn import metrics
 from matplotlib import pyplot as plt
 import numpy as np
-from data import X_t, target, target_test, X
+from data import X_t, target, target_test, X \
+    ,X_full, dataset, X_f_t
+
 
 clf = GradientBoostingClassifier(n_estimators=4000, learning_rate=0.001)
-clf.fit(X, target)
+# clf.fit(X, target)
+# print(metrics.classification_report(target_test, clf.predict(X_t)))
 
-print(metrics.classification_report(target_test, clf.predict(X_t)))
+
+# clf = RandomForestClassifier(n_jobs=4, n_estimators=10000, min_samples_leaf=3)
+# clf.fit(X, target)
+# print(metrics.classification_report(target_test, clf.predict(X_t)))
+
+clf.fit(X_full, dataset.Survived)
+from csv import DictWriter
+w = DictWriter(open('solve.csv', 'w'), fieldnames=['PassengerId', 'Survived'])
+w.writeheader()
+for i, x in enumerate(clf.predict(X_f_t)):
+    w.writerow({'PassengerId': i + 892, 'Survived': x})
+
+
+
 
 width = 0.35
 plt.bar(np.arange(len(clf.feature_importances_)), clf.feature_importances_, width=0.35)
 use_field = ['Pclass',
+             'Name',
              'Sex',
              'Age',
-             'SibSp',
-             'Parch',
+             'Title',
+             'SibSp+Parch',
+             'is_SibSp',
+             'is_3cl_man',
              'T1',
              'T2',
-             # 'T number',
+             'Cabin',
              'Fare',
              'Embarked']
 plt.xticks(np.arange(len(clf.feature_importances_)) + width/2., use_field)
